92ac4665170bde54a7fa239754e1ca2c788fc133
Jeniel Mangahis <jeniel.mangahis.work@gmail.com>
2019-07-01 16:30:57 +0800

    Added model and controler

---
+++ app/MailMessaging.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class MailMessaging extends Model
+{
+    protected $table = 'mail_messaging';
+    use SoftDeletes;
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function company()
+    {
+        return $this->belongsTo('App\Company');
+    }
+}
--- database/migrations/2019_07_01_082501_create_mail_messaging_table.php
+++ database/migrations/2019_07_01_082501_create_mail_messaging_table.php
@@ -22,6 +22,7 @@ class CreateMailMessagingTable extends Migration
             $table->string('cc');
             $table->string('bcc');
             $table->longText('content');
+            $table->integer('status');
             $table->timestamps();
             $table->softDeletes();
         });
